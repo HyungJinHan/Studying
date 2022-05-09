14277b2a5c0f2fa0aa2a97beebee516079920550
HyungJin Han <104360734+HyungJinHan@users.noreply.github.com>
2022-05-09 18:31:05 +0900

    Add files via upload

--- Python/create.py
+++ Python/create.py
@@ -2,7 +2,7 @@
 print("Content-type: text/html; charset=utf-8\n")
 # ↑ Python을 연동시켜서 화면에 도출될 수 있게 하는 코드 ↑ (1~2)
 
-import cgi, os #os라는 모듈로 묶여서 실행되는 명령어를 실행시킬 수 있음
+import cgi, os, View #os라는 모듈로 묶여서 실행되는 명령어를 실행시킬 수 있음
 
 form = cgi.FieldStorage()
 if "id" in form: #form안에 id 값이 있는 경우 else에 해당하는 값을 도출
@@ -11,20 +11,6 @@ if "id" in form: #form안에 id 값이 있는 경우 else에 해당하는 값을
 else:
     pageId = "Welcome"
     description = "Hello, Web"
-
-files = os.listdir("Data") #files라는 함수로 묶어서 Data 폴더에 있는 파일을 불러온다
-
-# for data in files:
-#     listStr = listStr + data #비어있는 문자열 + Data 내의 파일
-# print(listStr) #공백 + CSS + HTML + JavaScript + Python
-
-listStr = "" #비어있는 문자열 임의로 생성
-
-for item in files:
-    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
-#''작은 따옴표로 <li></li>를 묶어야 하나 봄
-# li 형식으로 item이라는 함수를 name으로 포맷팅을 한 후, 웹 주소도 id 값을 {name}으로 포맷팅,
-# li의 이름도 {name}으로 포맷팅해서 화면에 도출
 
 print('''<!doctype html>
 <html>
@@ -46,6 +32,6 @@ print('''<!doctype html>
   </form> <!-- 제목과 내용을 제출하기 위해 form으로 감싸기 -->
 </body>
 </html>
-'''.format(title=pageId, desc=description, listStr=listStr))
+'''.format(title=pageId, desc=description, listStr=View.getList()))
 
 # ?id=xxx -> 쿼리스트링

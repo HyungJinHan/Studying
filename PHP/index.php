6eeb5c47d3aecb20ef020e66cb610f12d97e3dd5
HyungJin Han <104360734+HyungJinHan@users.noreply.github.com>
2022-05-10 22:29:49 +0900

    추가적인 리팩토링 작업, 파일 불러오기 작업 실시

--- PHP/index.php
+++ PHP/index.php
@@ -1,40 +1,82 @@
+<?php
+  // 제목 함수 지정
+  function print_title() {
+    if(isset($_GET["id"])) {
+       // isset을 통해서 id값이 true인지 false인지 확인 후 id가 없는 false일 경우
+       // esle 값을 도출, id가 있는 true의 경우 if에 해당하는 값을 도출
+      echo $_GET["id"]; // URL의 id에 해당하는 값을 부제목으로 가져오기
+    }
+    else {
+      echo "Welcome!";
+    }
+  }
+
+ // 본문 함수 지정
+  function print_list() {
+    $list = scandir("./Data", 1);
+    // list라는 변수에 폴더 스캔 담기 (뒤의 숫자는 오름차, 내림차)
+    $i = 0; // i라는 변수를 0으로 지정
+    while($i < count($list)) {
+      if($list[$i] != ".") {
+        if($list[$i] != "..") {
+          // 불필요한 . .. 링크는 !=로 같지 않다는 조건을 걸어서 목록에 뜨지 않게 하기
+          echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+          // 태그가 깨질 수 있기 때문에 하이퍼링크 태그에 들어가는 "은 앞에
+          // \(역슬래시)를 입력해서 깨지지 않게 하기 (\" xxx \")
+        }
+      }
+      $i = $i + 1;
+    }
+    // i가 count를 통해 파일의 수를 알아내고 최종적으로 i가 알아낸
+    // 파일의 수보다 작으면 list 형식의 목록을 불러오다가 반복문 종료
+  }
+
+  // 목록 함수 지정
+  function print_description() {
+    if(isset($_GET["id"])) {
+      // isset을 통해서 id값이 true인지 false인지 확인 후 id가 없는 false일 경우
+      // esle 값을 도출, id가 있는 true의 경우 if에 해당하는 값을 도출
+      $file_get = file_get_contents("Data/".$_GET["id"]);
+      // Data 폴더와 .을 이용하여 결합 후 해당 URL의 id 값에 맞춰 내용 불러오기
+      echo nl2br($file_get); // 함수로 지정 후 자동 줄바꿈 설정
+    }
+    else {
+      $web_desc = "<p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.
+
+                  [1] English scientist Tim Berners-Lee invented the World Wide Web in 1989.
+
+                  He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.
+
+                  [2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+                  </p>";
+      echo nl2br($web_desc);
+    }
+  }
+?>
 <!doctype html>
 <html>
 <head>
-  <title>WEB1 - Welcome</title>
+  <title>
+    <?php
+      print_title(); // 위에서 지정한 제목 함수 출력
+    ?>
+  </title>
   <meta charset="utf-8">
 </head>
 <body>
   <h1><a href="index.php">WEB</a></h1>
-    <ol>
-      <li><a href="index.php?id=HTML">HTML</a></li>
-      <li><a href="index.php?id=CSS">CSS</a></li>
-      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
-    </ol>
+    <ul>
+      <?php
+        print_list(); // 위에서 지정한 본문 함수 출력
+      ?>
+    </ul>
   <h2>
     <?php
-      if(isset($_GET["id"])) {
-         // isset을 통해서 id값이 true인지 false인지 확인 후 id가 없는 false일 경우
-         // esle 값을 도출, id가 있는 true의 경우 if에 해당하는 값을 도출
-        echo $_GET["id"]; // URL의 id에 해당하는 값을 부제목으로 가져오기
-      }
-      else {
-        echo "Welcome!";
-      }
+      print_title(); // 위에서 지정한 제목 함수 출력
     ?>
   </h2>
     <?php
-      if(isset($_GET["id"])) {
-        // isset을 통해서 id값이 true인지 false인지 확인 후 id가 없는 false일 경우
-        // esle 값을 도출, id가 있는 true의 경우 if에 해당하는 값을 도출
-        $file_get = file_get_contents("Data/".$_GET["id"]);
-        // Data 폴더와 .을 이용하여 결합 후 해당 URL의 id 값에 맞춰 내용 불러오기
-        echo nl2br($file_get); // 함수로 지정 후 자동 줄바꿈 설정
-      }
-      else {
-        $file_web = file_get_contents("Data/WEB");
-        echo nl2br($file_web);
-      }
+      print_description(); // 위에서 지정한 목록 함수 출력
     ?>
 </body>
 </html>
--- PHP/php_data_types.php
+++ PHP/php_data_types.php
@@ -31,7 +31,7 @@
       echo strlen($str), "<br>\n"; // 함수 버전
       echo strlen("Hello, World!"), "<br>\n", "<br>\n"; // 일반 버전
 
-      echo "---------Variable (변수)---------", "<br>\n", "<br>\n";
+      echo "---------Variable (변수 $)---------", "<br>\n", "<br>\n";
       $a = 10; $b = 22;
       echo $a + $b, "<br>\n";
       $name1 = "한상현"; // 문자를 결합하기 위해 변수 앞뒤에 .이라는 결합 연산자 입력
@@ -55,7 +55,30 @@
       echo nl2br($htr), "<br>\n", "<br>\n", strlen($htr), "<br>\n", "<br>\n";
       // echo nl2br($xxx) : 엔터친 부분 자동 줄바꿈
 
-      echo "---------Comparison Operators (비교 연산자) &amp; Boolean Data Type (비교 연산자)---------", "<br>\n", "<br>\n";
+      function basic() { // 함수의 이름이 basic
+        print("hahahahahahaha <br>\n"); // 함수 호출 문법
+        print("hehehehehehehe <br>\n <br>\n");
+      }
+      basic();
+
+      function sum($left, $right) { // 좌우를 변수(parameter)로 지정
+        print($left + $right); // 좌우 변수를 더해서 출력하도록 지정
+        print("<br>\n"); // 함수 내에 줄바꿈 추가
+      }
+      sum(2, 4); // () 내부의 값(argument)에 따라 도출되는 결과가 다름
+      sum(10, 100);
+
+      echo "<br>\n", "<br>\n", "---------Return (출력)---------", "<br>\n", "<br>\n";
+      function sum2($left, $right) { // 변수를 좌우로 지정하고
+        return $left + $right; // 지정한 변수를 더해서 출력(return)하도록 지정
+        // return 다음으로 오는 함수는 무시됨 (return은 함수를 종료시킴)
+      }
+      print(sum2(4, 10));
+      print("<br>\n");
+      print(sum2(10, 87));
+      file_put_contents("result.txt", sum2(4, 10)); // print된 결과를 txt 파일에 저장
+
+      echo "<br>\n", "<br>\n", "---------Comparison Operators (비교 연산자) &amp; Boolean Data Type (비교 연산자)---------", "<br>\n", "<br>\n";
       echo ("1123"." → "); var_dump("1123"); echo("<br>\n");
       echo ("1123+5"." → "); var_dump(1123+5); echo("<br>\n");
       echo ("1==3"." → "); var_dump(1==3); echo("<br>\n");
@@ -71,7 +94,36 @@
       else {
         echo "2-2<br>";
       }
-      echo "3<br>";
+        echo "3<br>", "<br>\n";
+
+      echo "---------Loop (반복문 while)---------", "<br>\n", "<br>\n";
+      echo "1<br>";
+      $i = 0; // i라는 변수에 0이라는 값으로 지정
+      while ($i < 5) { // i의 값이 원하는 수에 도달하면 반복이 멈추게끔 지정
+        echo "2";
+        $i = $i + 1; // while이 실행 될 때마다 i에 1씩 더해진다
+      }
+      echo "<br>\n";
+      $t = 0;
+      while ($t <= 4) {
+        echo "3";
+        $t = $t + 1;
+      }
+
+      echo "<br>\n", "<br>\n", "---------Array (배열)---------", "<br>\n", "<br>\n";
+      $family = array("hsh", "jhs", "hhj1", "hhj2");
+      // family라는 변수에 hsh, jhs, hhj1, hhj2 담기
+      echo $family[2], "<br>\n"; // family내의 원하는 값을 도출
+      echo $family[1], "<br>\n";
+      echo count($family), "<br>\n"; // family라는 변수 내의 구성 요소의 수
+      array_push($family, "kws", "jhk", "kjh1", "kjh2"); // family라는 변수 내에 구성 요소를 끝에 추가
+      var_dump($family);
+
+      echo "<br>\n", "<br>\n", "---------Array Combine (배열 합치기)---------", "<br>\n", "<br>\n";
+      $hanfamily = array("hsh", "jhs", "hhj1", "hhj2");
+      $rules = array("father", "mother", "big son", "small son");
+      $home = array_combine($hanfamily, $rules);
+      var_dump($home);
     ?>
   </body>
 </html>

d046ebcfaf65ac56b61643eb6eb27b00f132b3c5
HyungJin Han <104360734+HyungJinHan@users.noreply.github.com>
2022-05-30 20:51:44 +0900

    Delete 연습장/Python&Ruby directory

--- 연습장/Python&Ruby/python_in&out.py
+++
@@ -1,7 +0,0 @@
-#!python
-print("Content-type: text/html; charset=utf-8\n")
-# ↑ Python을 연동시켜서 화면에 도출될 수 있게 하는 코드 ↑ (1~2)
-
-in_str = input("입력해주세요.\n")
-print(in_str.capitalize() + " World")
-# ex : hello 입력 → Hello World
